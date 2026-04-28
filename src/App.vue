43c8b7c75a8d99658e93f7085dacb77cd3dacd35
juliabittencourt30 <bittjuliarosa@gmail.com>
2026-04-28 11:57:48 -0300

    continuação

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import ProdutoChild from './components/ProdutoChild.vue';
+import ButtonChild from './components/ButtonChild.vue';
   const produtos = ref([
 { id: 1, nome: 'Ração Premium Cães', preco: 120, categoria: 'Alimentos' },
 { id: 2, nome: 'Ração Gatos Castrados', preco: 95, categoria: 'Alimentos' },
@@ -12,16 +13,36 @@ import ProdutoChild from './components/ProdutoChild.vue';
 { id: 8, nome: 'Coleira Azul', preco: 28, categoria: 'Acessórios' },
 { id: 9, nome: 'Guia de Passeio', preco: 40, categoria: 'Acessórios' }
 ])
+
+const preco = ref(0);
+const posicaoProduto = ref (-1);
+const alterando = ref(false);
+
+function corrigirPreco(id) {
+  posicaoProduto.value = produtos.value.findIndex(p => p.id === id);
+  preco.value = produtos.value[posicaoProduto.value].preco;
+  alterando.value = true;
+}
+
+function salvarPreco() {
+  produtos.value[posicaoProduto.value].preco = preco.value;
+  alterando.value = false;
+}
 </script>
 
 <template>
   <div class="container">
     <ul>
       <ProdutoChild v-for="produto in produtos" :key="produto.id" :id="produto.id" :nome="produto.nome" :preco="produto.preco" 
-      :categoria="produto.categoria">
+      :categoria="produto.categoria" @corrigirpreco="corrigirPreco">
 
       </ProdutoChild>
     </ul>
+    <div v-show="alterando">
+      <label>Preço</label>
+      <input type="number" v-model.number="preco">
+      <ButtonChild @clique="salvarPreco">Salvar</ButtonChild>
+    </div>
   </div>
 </template>
 
