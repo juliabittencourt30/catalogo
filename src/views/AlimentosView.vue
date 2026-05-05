9cb3ef5386de2f8718c2bf1953b319732cf0128e
juliabittencourt30 <bittjuliarosa@gmail.com>
2026-05-05 11:58:55 -0300

    continuação

---
+++ src/views/AlimentosView.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import { ref } from 'vue'
+    import { listaProdutos } from '@/data/produtos';
+    const produtos = ref(listaProdutos)
+    produtos.value = produtos.value.filter(p => p.categoria === 'Alimentos')
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
