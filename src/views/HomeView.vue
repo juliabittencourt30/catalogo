835e3d50168a1dfca8102b75b9c37f5683e00830
juliabittencourt30 <bittjuliarosa@gmail.com>
2026-05-08 15:02:46 -0300

    continuação

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <router-link to="/">Home</router-link>
+        <router-link to="/alimentos">Alimentos</router-link>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
